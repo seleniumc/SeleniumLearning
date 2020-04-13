01917225cd6305d6b246dc169c7ef691cc575b71
Murali Krishna <v-mukri@microsoft.com>
2020-04-13 16:03:33 +0530

    13/4/2020 Class Samples

---
+++ CSharpTraining/CSharpTraining/Arrays/ArraySamples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpTraining.Arrays
+{
+    class ArraySamples
+    {
+        string[] aaa = { "\"arr\"", "base\t", "c\n", "100" };
+        int[] c = { 1, 2, 3, 4 };
+        char[] d = { 'a', 'b', 'n' };
+        bool[] b = { true, false, true };
+        public void ArraySamplesMethod()
+        {
+            ArraySamples[] e1 = { new ArraySamples(), new ArraySamples(), new ArraySamples() };
+            string[] abc=(string[])aaa.Clone();
+            int[] acbd = (int[])c.Clone();
+            Console.WriteLine(acbd.Max() + "\t" + acbd.Min()+"\t"+acbd.Average()) ;
+            Console.WriteLine(""+(aaa==abc)+"\t"+abc.GetHashCode()+"\t"+aaa.GetHashCode());
+            foreach (string a in aaa)
+            {
+                try
+                {
+                    Convert.ToInt32(a);
+                    Console.WriteLine("Valid number");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid number " + e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ CSharpTraining/CSharpTraining/StaticAndObjectVariables/StcObjVariables.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTraining.StaticAndObjectVariables
+{
+    class StcObjVariables
+    {
+        public static int a = 5;
+        public int b = 6;
+        public void ContinueAndBreak()
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (j == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(j);
+            }
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+                if (i == 4)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}

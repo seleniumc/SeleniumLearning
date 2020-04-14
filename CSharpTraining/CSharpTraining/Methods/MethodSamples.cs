532e56578980d19c97be1bcb3465408512903558
Murali Krishna <v-mukri@microsoft.com>
2020-04-14 16:45:45 +0530

    14th April Class

---
+++ CSharpTraining/CSharpTraining/Methods/MethodSamples.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTraining.Methods
+{
+    class MethodSamples
+    {
+        public void SimpleMethod()
+        {
+            Console.WriteLine("This is a simple method");
+            SimpleIntMethod();
+            SimpleAdd(1,4);
+        }
+
+        public int SimpleIntMethod()
+        {
+            return 1;
+        }
+
+        public static int SimpleAdd(int a, int b)
+        {
+            Sample();
+            return a + b;
+        }
+
+        public static void Sample()
+        {
+            SimpleAdd(4,1);
+        }
+
+        public string SimpleStringMethod(string a)
+        {
+            Console.WriteLine("This is simple string method with string parameter");
+            return a;
+        }
+
+        public int SimpleStringMethod(string a,string b,int c)
+        {
+            Console.WriteLine("This is simple string method with string parameter");
+            return c;
+        }
+
+        public string SimpleStringMethod(string a,string b)
+        {
+            Console.WriteLine("This is simple string method with string parameter");
+            return a+b;
+        }
+
+        public MethodSamples ReturnObject()
+        {
+            return new MethodSamples();
+        }
+
+        public MethodSamples()
+        {
+            Console.WriteLine("");
+        }
+
+       public MethodSamples(int a)
+        {
+
+        }
+
+        public MethodSamples(int a,int b)
+        {
+
+        }
+        public void MyMethod(string country = "Norway")
+        {
+            Console.WriteLine(country);
+        }
+
+    }
+}
--- CSharpTraining/CSharpTraining/Program.cs
+++ CSharpTraining/CSharpTraining/Program.cs
@@ -2,6 +2,7 @@
 using CSharpTraining.Conditions;
 using CSharpTraining.Inheritance;
 using CSharpTraining.Loops;
+using CSharpTraining.Methods;
 using CSharpTraining.Operators;
 using CSharpTraining.Polymarphism;
 using CSharpTraining.StaticAndObjectVariables;
@@ -15,60 +16,60 @@ namespace CSharpTraining
     {
         static void Main(string[] args)
         {
-            /* Program p = new Program();
-             p.BaseClassMethod();
+            Program p = new Program();
+            p.BaseClassMethod();
 
-             //Method overloading
-             MethodOverLoading mol = new MethodOverLoading();
-             mol.Add(1, 2);
-             mol.Add(1, 2.3);
+            //Method overloading
+            MethodOverLoading mol = new MethodOverLoading();
+            mol.Add(1, 2);
+            mol.Add(1, 2.3);
 
-             //Method overriding
-             MORSubClass mor = new MORSubClass();
-             mor.SuperClassMethod();
+            //Method overriding
+            MORSubClass mor = new MORSubClass();
+            mor.SuperClassMethod();
 
-             //Variables sample
-             VariableSamples vs = new VariableSamples();
-             vs.SampleMethod();
+            //Variables sample
+            VariableSamples vs = new VariableSamples();
+            vs.SampleMethod();
 
-             //Input reading
-             InputReading ir = new InputReading();
-             ir.ReadInteger();
+            //Input reading
+            InputReading ir = new InputReading();
+            ir.ReadInteger();
 
-             //Operators sample
-             OperatorsSample os = new OperatorsSample();
-             os.Modulus(4, 3);
-             int a = 10;
-             a %= 5;
-             Console.WriteLine(a);
-             Console.WriteLine(os.IfAGreaterThanB(1, 6));
-             Console.WriteLine(os.IfAEqualsB(1, 1));
+            //Operators sample
+            OperatorsSample os = new OperatorsSample();
+            os.Modulus(4, 3);
+            int a = 10;
+            a %= 5;
+            Console.WriteLine(a);
+            Console.WriteLine(os.IfAGreaterThanB(1, 6));
+            Console.WriteLine(os.IfAEqualsB(1, 1));
 
-             int b = (1 == 2) ? 2 : 1;
-             Console.WriteLine(b);
-             b = (1 == 1) ? 2 : 1;
-             Console.WriteLine(b);
+            int b = (1 == 2) ? 2 : 1;
+            Console.WriteLine(b);
+            b = (1 == 1) ? 2 : 1;
+            Console.WriteLine(b);
 
-             Console.WriteLine(Math.Floor(12.1));
-             Console.WriteLine(Math.Ceiling(12.1));
+            Console.WriteLine(Math.Floor(12.1));
+            Console.WriteLine(Math.Ceiling(12.1));
 
-             //String samples
-             StringSamples ss = new StringSamples();
-             string firstName = "John";
-             string lastName = "Doe";
-             string name = $"My full name is: {firstName} {lastName}";
-             Console.WriteLine(name);
+            //String samples
+            StringSamples ss = new StringSamples();
+            string firstName = "John";
+            string lastName = "Doe";
+            string name = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(name);
 
-             //Condition samples
-             ConditionSample cs = new ConditionSample();
-             cs.Conditions();
+            //Condition samples
+            ConditionSample cs = new ConditionSample();
+            cs.Conditions();
 
-             //Loop samples
-             LoopSamples ls = new LoopSamples();
-             ls.Loop();*/
+            //Loop samples
+            LoopSamples ls = new LoopSamples();
+            ls.Loop();
 
             //Statci variable samples
-            Console.WriteLine("Static variable value is "+StcObjVariables.a);
+            Console.WriteLine("Static variable value is " + StcObjVariables.a);
             StcObjVariables stcObj = new StcObjVariables(), stcObj1 = new StcObjVariables();
             Console.WriteLine("Object1 variable value is " + stcObj.b);
             Console.WriteLine("Object2 variable value is " + stcObj1.b);
@@ -81,6 +82,15 @@ namespace CSharpTraining
             //ArraySamples
             new ArraySamples().ArraySamplesMethod();
 
+            //Method Samples
+            MethodSamples ms = new MethodSamples();
+            MethodSamples ms1 = new MethodSamples(2);
+            MethodSamples ms2 = new MethodSamples(1, 2);
+            int a1 = ms2.SimpleIntMethod();
+            string b1 = ms2.SimpleStringMethod("a");
+            int v = MethodSamples.SimpleAdd(1, 3);
+            ms2.MyMethod();
+            ms2.MyMethod("India");
         }
 
         public Program()

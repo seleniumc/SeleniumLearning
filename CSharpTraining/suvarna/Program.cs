cb7818e9805cc9ee0a6b9bbbf7c7660ce7be3a1e
suvarna <kodhamasimahamsuvarna@gmail.com>
2020-04-15 11:06:33 +0530

    adding my practice project

---
+++ CSharpTraining/suvarna/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace suvarna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = "suvarna";
+            Console.WriteLine(name);
+            int myNum = 15;
+            myNum = 20;
+            Console.WriteLine(myNum);
+            string myText = "Hello";
+            Console.WriteLine(myText);
+
+        }
+    }
+}
---
+++ CSharpTraining/suvarna/inheritance/inheritancesample.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace suvarna.inheritance
+{
+    class InheritanceSample
+    {
+    }
+}

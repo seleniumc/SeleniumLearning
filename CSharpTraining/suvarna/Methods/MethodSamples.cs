fe98a39ced7cfcc42cb1457e0b1781fdeb317b33
suvarna <kodhamasimahamsuvarna@gmail.com>
2020-04-16 16:11:52 +0530

    16th session

---
+++ CSharpTraining/suvarna/Methods/MethodSamples.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace suvarna.Methods
+{
+    class MethodSamples
+    {
+        public void SimpleMethod()
+        {
+            Console.WriteLine("This is Simple method");
+        }
+        public int SimpleIntMethod()
+        {
+            return 2;
+           
+        }
+    }
+}

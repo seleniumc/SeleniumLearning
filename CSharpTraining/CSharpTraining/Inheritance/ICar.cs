5cf81701e933d64204b6b26bee0c73fe2f658464
Murali Krishna <v-mukri@microsoft.com>
2020-05-18 08:42:21 +0530

    Class 18/05/2020 Back to basics

---
+++ CSharpTraining/CSharpTraining/Inheritance/ICar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTraining.Inheritance
+{
+    interface ICar
+    {
+        public string getMake();
+        public string getModel();
+        public string getYear();
+        public string getBatchNumber();
+    }
+}
